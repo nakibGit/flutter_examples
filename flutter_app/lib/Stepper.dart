31c6056e3dadb660209c07bc7744b53f470ff40f
Nakib Hossain <nakib143048@gmail.com>
2021-02-20 01:02:04 +0600

    Flutter Custom Stepper initial Commit

---
+++ flutter_app/lib/Stepper.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+class NumberStepper extends StatelessWidget {
+  final double width;
+  final totalSteps;
+  final int curStep;
+  final Color stepCompleteColor;
+  final Color currentStepColor;
+  final Color inactiveColor;
+  final double lineWidth = 3.5;
+  NumberStepper({
+    Key key,
+    @required this.width,
+    @required this.curStep,
+    @required this.stepCompleteColor,
+    @required this.totalSteps,
+    @required this.inactiveColor,
+    this.currentStepColor,
+  })  : assert(curStep > 0 == true && curStep <= totalSteps + 1),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 20.0,
+        left: 24.0,
+        right: 24.0,
+      ),
+      width: this.width,
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: _steps(),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
+  }
+
+  getCircleColor(i) {
+    var color;
+    if (i + 1 < curStep) {
+      color = stepCompleteColor;
+    } else if (i + 1 == curStep)
+      color = currentStepColor;
+    else
+      color = Colors.white;
+    return color;
+  }
+
+  getBorderColor(i) {
+    var color;
+    if (i + 1 < curStep) {
+      color = stepCompleteColor;
+    } else if (i + 1 == curStep)
+      color = currentStepColor;
+    else
+      color = inactiveColor;
+
+    return color;
+  }
+
+  getLineColor(i) {
+    var color =
+        curStep > i + 1 ? Colors.blue.withOpacity(0.4) : Colors.grey[200];
+    return color;
+  }
+
+  List<Widget> _steps() {
+    var list = <Widget>[];
+    for (int i = 0; i < totalSteps; i++) {
+      //colors according to state
+
+      var circleColor = getCircleColor(i);
+      var borderColor = getBorderColor(i);
+      var lineColor = getLineColor(i);
+
+      list.add(
+        Container(
+          width: 28.0,
+          height: 28.0,
+          child: getInnerElementOfStepper(i),
+          decoration: new BoxDecoration(
+            color: circleColor,
+            borderRadius: new BorderRadius.all(new Radius.circular(25.0)),
+            border: new Border.all(
+              color: borderColor,
+              width: 1.0,
+            ),
+          ),
+        ),
+      );
+
+      //line between icons
+      if (i != totalSteps - 1) {
+        list.add(
+          Expanded(
+            child: Container(
+              height: lineWidth,
+              color: lineColor,
+            ),
+          ),
+        );
+      }
+    }
+
+    return list;
+  }
+
+  Widget getInnerElementOfStepper(index) {
+    if (index + 1 < curStep) {
+      return Icon(
+        Icons.check,
+        color: Colors.white,
+        size: 16.0,
+      );
+    } else if (index + 1 == curStep) {
+      return Center(
+        child: Text(
+          '$curStep',
+          style: TextStyle(
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Roboto',
+          ),
+        ),
+      );
+    } else
+      return Container();
+  }
+}
---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+import 'Stepper.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Stepper Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentStep = 1;
+  int stepLength = 5;
+  bool complete;
+
+  next() {
+    if (currentStep <= stepLength) {
+      goTo(currentStep + 1);
+    }
+  }
+
+  back() {
+    if (currentStep > 1) {
+      goTo(currentStep - 1);
+    }
+  }
+
+  goTo(int step) {
+    setState(() => currentStep = step);
+    if (currentStep > stepLength) {
+      setState(() => complete = true);
+    }
+    //print(currentStep);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          NumberStepper(
+            totalSteps: stepLength,
+            width: MediaQuery.of(context).size.width,
+            curStep: currentStep,
+            stepCompleteColor: Colors.blue,
+            currentStepColor: Color(0xffdbecff),
+            inactiveColor: Color(0xffbababa),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            child: currentStep <= stepLength
+                ? Text(
+                    "Step $currentStep",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.blue,
+                    ),
+                  )
+                : Text(
+                    "Completed!",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.blue,
+                    ),
+                  ),
+          )
+        ],
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: RaisedButton(
+              disabledColor: Colors.grey[50],
+              onPressed: currentStep == 1
+                  ? null
+                  : () {
+                      back();
+                    },
+              child: Text('Back'),
+            ),
+          ),
+          Expanded(
+            child: RaisedButton(
+              onPressed: () {
+                next();
+              },
+              child: Text(
+                currentStep == stepLength ? 'Finish' : 'Next',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.blue,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
